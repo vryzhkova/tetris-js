8f9fc6ad7ce70aa95f1a4ff8b8c72529d92de835
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-05-01 15:12:42 +0400

    added viewArea

--- index.js
+++ index.js
@@ -6,34 +6,65 @@ const game = {
     area: [
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'x', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'x', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'x', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'o', 'o'],
+        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'x'],
+        ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'o', 'x'],
+        ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'x', 'x'],
     ],
-    activeTetromino() {},
 
-    moveLeft() {},
+    activeTetromino: {
+        x: 3,
+        y: 0,
+        block: [
+            ['o', 'x', 'o'],
+            ['o', 'x', 'o'],
+            ['x', 'x', 'o'],
+        ]
+    },
 
-    moveRight() {},
+    moveLeft() {
+        this.activeTetromino.x -= 1;
+    },
 
-    moveDown() {},
+    moveRight() {
+        this.activeTetromino.x += 1;
+    },
+
+    moveDown() {
+        this.activeTetromino.y += 1;
+    },
 
     rotateTetromino() {},
+
+    get viewArea() {
+        const area = JSON.parse(JSON.stringify(this.area));
+        const {x, y, block: tetromino } = this.activeTetromino;
+
+        for(let i = 0; i < tetromino.length; i++) {
+            const row = tetromino[i];
+
+            for (let j = 0; j < row.length; j++) {
+                if (row[j] === 'x') {
+                    area[y + i][x + j] = tetromino[i][j];
+                }
+            }
+        }
+        return area;
+    }
 }
 
 //  отрисовка
@@ -65,4 +96,4 @@ const showArea = (area) => {
 
 };
 
-showArea(game.area);
+showArea(game.viewArea);
