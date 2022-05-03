a7f3c673123cd7bd0f6d122870e01311e81d2b74
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-05-03 13:53:21 +0400

    added lesson 3

--- index.js
+++ index.js
@@ -1,162 +1,37 @@
+import { Game } from "./modules/game.js";
+
+const game = new Game;
+
+game.createTetromino();
+
 const SIZE_BLOCK = 30;
-
-//  механика
-
-const game = {
-    area: [
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'x'],
-        ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'o', 'x'],
-        ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'x', 'x'],
-    ],
-
-    activeTetromino: {
-        x: 3,
-        y: 0,
-        block: [
-            ['o', 'x', 'o'],
-            ['o', 'x', 'o'],
-            ['x', 'x', 'o'],
-        ],
-        rotationIndex: 0,
-        rotation: [
-            [
-                ['o', 'x', 'o'],
-                ['o', 'x', 'o'],
-                ['x', 'x', 'o'],
-            ],
-            [
-                ['x', 'o', 'o'],
-                ['x', 'x', 'x'],
-                ['o', 'o', 'o'],
-            ],
-            [
-                ['o', 'x', 'x'],
-                ['o', 'x', 'o'],
-                ['o', 'x', 'o'],
-            ],
-            [
-                ['o', 'o', 'o'],
-                ['x', 'x', 'x'],
-                ['o', 'o', 'x'],
-            ],
-        ],
-    },
-
-    moveLeft() {
-        if(this.checkOutPosition(this.activeTetromino.x - 1, this.activeTetromino.y)) {
-            this.activeTetromino.x -= 1;
-        }  
-    },
-
-    moveRight() {
-        if(this.checkOutPosition(this.activeTetromino.x + 1, this.activeTetromino.y)) {
-            this.activeTetromino.x += 1;
-        }
-    },
-
-    moveDown() {
-        if(this.checkOutPosition(this.activeTetromino.x, this.activeTetromino.y + 1)) {
-            this.activeTetromino.y += 1;
-        } else {
-            this.stopMove();
-        }
-    },
-
-    rotateTetromino() {
-        this.activeTetromino.rotationIndex = 
-            this.activeTetromino.rotationIndex < 3 ? 
-                this.activeTetromino.rotationIndex + 1 : 0;
-
-            this.activeTetromino.block = 
-                this.activeTetromino.rotation[this.activeTetromino.rotationIndex];
-
-                if(!this.checkOutPosition(this.activeTetromino.x, this.activeTetromino.y)) {
-                    this.activeTetromino.rotationIndex = 
-                        this.activeTetromino.rotationIndex > 0 ? 
-                            this.activeTetromino.rotationIndex - 1 : 3;
-        
-                    this.activeTetromino.block = 
-                        this.activeTetromino.rotation[this.activeTetromino.rotationIndex];
-                }
-    },
-
-    get viewArea() {
-        const area = JSON.parse(JSON.stringify(this.area));
-        const {x, y, block: tetromino } = this.activeTetromino;
-
-        for(let i = 0; i < tetromino.length; i++) {
-            const row = tetromino[i];
-
-            for (let j = 0; j < row.length; j++) {
-                if (row[j] === 'x') {
-                    area[y + i][x + j] = tetromino[i][j];
-                }
-            }
-        }
-        return area;
-    },
-
-    checkOutPosition(x, y) {
-        const tetromino = this.activeTetromino.block;
-        for(let i = 0; i < tetromino.length; i++) {
-            for (let j = 0; j < tetromino[i].length; j++) {
-                if(tetromino[i][j] === 'o') continue;
-
-                if(!this.area[y + i] || !this.area[y + i][x + j] || this.area[y + i][x + j] === 'x') {
-                    return false;
-                }
-
-            }
-        }
-        return true;
-    },
-
-    stopMove() {
-        const {x, y, block: tetromino } = this.activeTetromino;
-
-        for(let i = 0; i < tetromino.length; i++) {
-            const row = tetromino[i];
-            for (let j = 0; j < row.length; j++) {
-                if (row[j] === 'x') {
-                    this.area[y + i][x + j] = tetromino[i][j];
-                }
-            }
-        }
-    }
-}
+export const COLUMNS = 10;
+export const ROWS = 20;
 
 //  отрисовка
 
 const container = document.querySelector('.container');
 
+const colors = {
+    J: 'FireBrick',
+    I: 'CadetBlue',
+    O: 'Gold',
+    L: 'StateBlue',
+    2: 'RoyalBlue',
+    T: 'Indigo',
+    S: 'MediumSeaGreen',
+}
+
 const canvas = document.createElement('canvas');
 canvas.classList.add('game-area');
 container.append(canvas);
-canvas.width = SIZE_BLOCK * 10;
-canvas.height = SIZE_BLOCK * 20;
+canvas.width = SIZE_BLOCK * COLUMNS;
+canvas.height = SIZE_BLOCK * ROWS;
 
 const context = canvas.getContext('2d');
 
 const showArea = (area) => {
     context.clearRect(0, 0, canvas.width, canvas.height);
-
 
     for(let y = 0; y < area.length; y++) {
         const line = area[y];
@@ -164,7 +39,7 @@ const showArea = (area) => {
         for(let x = 0; x < line.length; x++) {
             const block = line[x];
             if(block === 'x') {
-                context.fillStyle = 'tomato';
+                context.fillStyle = colors[block];
                 context.strokeStyle = 'white';
                 context.fillRect(x * SIZE_BLOCK, y * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK);
                 context.strokeRect(x * SIZE_BLOCK, y * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK);
